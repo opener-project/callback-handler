4fa0ffc15c05cebf86921d691fcf7cc93d7b2efc
Yorick Peterse <yorickpeterse@olery.com>
2014-11-12 15:51:13 +0100

    Basic docs for the Http strategy.

--- lib/opener/callback_handler/strategies/http.rb
+++ lib/opener/callback_handler/strategies/http.rb
@@ -1,17 +1,31 @@
 module Opener
   class CallbackHandler
     module Strategies
+      ##
+      # Strategy for submitting data to a regular HTTP URL.
+      #
       class Http
-
+        ##
+        # @param [String] url
+        # @return [TrueClass|FalseClass]
+        #
         def self.pass_validation?(url)
           !!Regexp.new("^https?:\/\/.*").match(url)
         end
 
+        ##
+        # @param [String] url
+        # @param [Hash] params
+        #
         def process(url, params = {})
           body_params = params.keys.include?(:body) ? params : {:body => params}
+
           http_client.post_async(url, body_params)
         end
 
+        ##
+        # @return [HTTPClient]
+        #
         def http_client
           client = ::HTTPClient.new
           client.connect_timeout = 120
