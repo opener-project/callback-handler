c1381c8aba63f3b394a1b9adca7fb3cf63a6293c
Yorick Peterse <yorickpeterse@olery.com>
2014-11-17 12:47:17 +0100

    Rewrote the HTTP strategy.
    This strategy now _always_ outputs JSON while making sure the correct
    Content-Type header is set. This ensures it can be used properly with the
    daemons without having to use some kind of crazy Rack compatible output format.
    
    Also, tests!

--- lib/opener/callback_handler/strategies/http.rb
+++ lib/opener/callback_handler/strategies/http.rb
@@ -2,15 +2,35 @@ module Opener
   class CallbackHandler
     module Strategies
       ##
-      # Strategy for submitting data to a regular HTTP URL.
+      # Strategy for submitting data to a regular HTTP URL. This strategy
+      # submits data as JSON while making sure the correct Content-Type header
+      # is set.
+      #
+      # @!attribute [r] http
+      #  @return [HTTPClient]
       #
       class Http
+        attr_reader :http
+
+        ##
+        # The headers to use when submitting data.
+        #
+        # @return [Hash]
+        #
+        HEADERS = {
+          'Content-Type' => 'application/json'
+        }
+
         ##
         # @param [String] url
         # @return [TrueClass|FalseClass]
         #
         def self.pass_validation?(url)
-          !!Regexp.new("^https?:\/\/.*").match(url)
+          return !!Regexp.new("^https?:\/\/.+").match(url)
+        end
+
+        def initialize
+          @http = HTTPClient.new
         end
 
         ##
@@ -18,18 +38,9 @@ module Opener
         # @param [Hash] params
         #
         def process(url, params = {})
-          body_params = params.keys.include?(:body) ? params : {:body => params}
+          body = JSON.dump(params)
 
-          http_client.post_async(url, body_params)
-        end
-
-        ##
-        # @return [HTTPClient]
-        #
-        def http_client
-          client = HTTPClient.new
-
-          return client
+          http.post(url, :body => body, :header => HEADERS)
         end
       end # Http
     end # Strategies
---
+++ spec/opener/callback_handler/strategies/http_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Opener::CallbackHandler::Strategies::Http do
+  context 'pass_validation?' do
+    example 'return true for an HTTP URL' do
+      described_class.pass_validation?('http://foo.com').should == true
+    end
+
+    example 'return true for an HTTPS URL' do
+      described_class.pass_validation?('https://foo.com').should == true
+    end
+
+    example 'return false for a URL with just a scheme' do
+      described_class.pass_validation?('http://').should == false
+    end
+  end
+
+  context '#process' do
+    before do
+      @strategy = described_class.new
+    end
+
+    example 'send a message to the callback URL' do
+      url     = 'http://foo.com'
+      payload = JSON.dump(:number => 10)
+
+      @strategy.http.should_receive(:post)
+        .with(url, :body => payload, :header => described_class::HEADERS)
+
+      @strategy.process(url, :number => 10)
+    end
+  end
+end
